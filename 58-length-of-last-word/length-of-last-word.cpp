457513ce8fe20c01855be2713c9411e44094392a
Akhildas-44 <ngakhil.das@outlook.com>
2026-08-13 15:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (8.90%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size() -1;
+        int count=0;
+        while(n>=0)
+        {
+            if(s[n]!=' ')
+            {
+                count++;
+            }
+            else if(count>0)
+            {
+                break;
+            }
+            n--;
+        }
+        return count;
+    }
+};
